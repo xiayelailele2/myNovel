ddeacfdddf523e817d5f3bead8a2586656003ea4
wakaka <2321352131@qq.com>
2025-11-26 14:26:10 +0800

    change01

--- src/main/java/io/github/xxyopen/novel/NovelApplication.java
+++ src/main/java/io/github/xxyopen/novel/NovelApplication.java
@@ -25,7 +25,7 @@ import java.util.Map;
 @EnableScheduling
 @Slf4j
 public class NovelApplication {
-
+//增加一些注释，用来测试讲变化调整至其他分支。
     public static void main(String[] args) {
         SpringApplication.run(NovelApplication.class, args);
     }
